06afdbdc4454d976166b6b0585134aaf741a0e7b
epicdigitalacademy <131464258+epicdigitalacademy@users.noreply.github.com>
2024-12-25 20:23:45 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,16 +12,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute();
 
 app.Run();
