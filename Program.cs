7fcb3cd556df974d01d32d728b051bce2b9c7e4d
epicdigitalacademy <131464258+epicdigitalacademy@users.noreply.github.com>
2024-12-25 20:36:05 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,18 +7,20 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-                {
-                    app.UseSwagger();
-                    app.UseSwaggerUI();
-                }
+ {
+   app.UseSwagger();
+   app.UseSwaggerUI();
+ }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
